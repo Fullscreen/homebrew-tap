47db541e0dee4d2c5431a5b8e0fde10732cf3821
Drew Stokes <drew@drewstokes.com>
2016-08-25 10:49:39 -0700

    add dkv formula

---
+++ dkv.rb
@@ -0,0 +1,11 @@
+class Dkv < Formula
+  desc ""
+  homepage "https://github.com/Fullscreen/dkv"
+  url "https://github.com/Fullscreen/dkv/releases/download/1.0.0/darwin_amd64.zip"
+  version "1.0.0"
+  sha256 "1c75bc703309beeb91da73f1fa6564244b0e154039441717ad1a15cfdb7b207c"
+
+  def install
+    bin.install "dkv"
+  end
+end
