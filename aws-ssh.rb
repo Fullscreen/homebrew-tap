84a4a8c5c5b57e9bc99b0b8e24651a4925fd61f7
Drew Stokes <drew@drewstokes.com>
2016-08-29 11:58:05 -0700

    aws-ssh 1.0.2

--- aws-ssh.rb
+++ aws-ssh.rb
@@ -5,9 +5,9 @@
 class AwsSsh < Formula
   desc "Convenience bash function for SSH on EC2"
   homepage "https://github.com/dstokes/aws-ssh"
-  url "https://github.com/dstokes/aws-ssh/archive/1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "5304581b6288e34c66ef33eb5dc7c36271b2e188c99eba82cc4e5fd29cfc0153"
+  url "https://github.com/dstokes/aws-ssh/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "b42ded4ac288f0d5ba0aae7a441b7f3eb6a0fae78986556210889dea892a73b2"
 
   def install
     bin.install "aws-ssh"
