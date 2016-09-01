44e6818531027c1fba430944fab2375c947f8949
Drew Stokes <drew@drewstokes.com>
2016-09-01 10:55:03 -0700

    dkv 1.0.1

--- dkv.rb
+++ dkv.rb
@@ -1,9 +1,9 @@
 class Dkv < Formula
   desc "A command-line interface to a DynamoDB backed key-value store"
   homepage "https://github.com/Fullscreen/dkv"
-  url "https://github.com/Fullscreen/dkv/releases/download/1.0.0/darwin_amd64.zip"
-  version "1.0.0"
-  sha256 "1c75bc703309beeb91da73f1fa6564244b0e154039441717ad1a15cfdb7b207c"
+  url "https://github.com/Fullscreen/dkv/releases/download/1.0.1/darwin_amd64.zip"
+  version "1.0.1"
+  sha256 "7515a649ec48b2cd1715c84dd5124f261ead60605137311100608d2b5d1f0c27"
 
   def install
     bin.install "dkv"
