d444692d7da295f2fbdd2e47a1b42c64913b8738
Drew Stokes <drew@drewstokes.com>
2016-10-07 16:30:36 -0700

    dynamo-env 1.0.1

--- dynamo-env.rb
+++ dynamo-env.rb
@@ -1,9 +1,9 @@
 class DynamoEnv < Formula
   desc "Access DynamoDB key value pairs through a `/usr/bin/env` interface"
   homepage "https://github.com/Fullscreen/dynamo-env"
-  url "https://github.com/Fullscreen/dynamo-env/releases/download/1.0.0/darwin_amd64.zip"
-  version "64"
-  sha256 "036c502e0f89243712c66a7cc9f048ec3c557ae9ee5a680b59465f98883ccf07"
+  url "https://github.com/Fullscreen/dynamo-env/releases/download/1.0.1/darwin_amd64.zip"
+  version "1.0.1"
+  sha256 "30da46f2a8867efaa92ac0348e5d5f87e605d8efd46d5b1c0a92d49d434c0087"
 
   def install
     bin.install "dynamo-env"
