51ff80e42ec50a9113747f7fcd0d760ade8843a7
Scott Stout <scott.stout@fullscreen.net>
2016-10-26 15:14:08 -0700

    garchiver tap

---
+++ garchiver.rb
@@ -0,0 +1,12 @@
+class Garchiver < Formula
+  desc "bash wrapper for GAM and GVault"
+  homepage "https://github.com/skout23/garchiver"
+  url "https://github.com/skout23/garchiver/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "832a746027c87143ccf91ae7c041c754d16c75257a4dc4f467bb964ea0fd80ee"
+
+  def install
+    bin.install "garchiver"
+  end
+
+end
