f470cdd9c1647901ac469e93818ad8cb8860e41d
Borislav Ivanov <borogl@gmail.com>
2016-12-12 16:46:59 -0800

    fs-env version 1.1.0

--- fs-env.rb
+++ fs-env.rb
@@ -3,8 +3,8 @@ class FsEnv < Formula
   Fullscreen applications"
   homepage "https://github.com/Fullscreen/fs-env"
   url "https://github.com/Fullscreen/fs-env/releases/download/1.0.0/darwin_amd64.zip"
-  version "1.0.0"
-  sha256 "ea735aed97c63e595e6bc1719594b1404da30cb2b53f75c5fd43820859f55bf6"
+  version "1.1.0"
+  sha256 "70b7682ba2ccd06d1a13e8d574aa1de4db1f820c0a2008963bbf221288162f62"
 
   def install
     bin.install "fs-env"
