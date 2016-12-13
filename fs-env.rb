aaa42f1eae28c3969c19f081644345a1ef34a82a
Borislav Ivanov <borogl@gmail.com>
2016-12-12 16:54:36 -0800

    fs-env version 1.1.0

--- fs-env.rb
+++ fs-env.rb
@@ -2,7 +2,7 @@ class FsEnv < Formula
   desc "A command-line interface to a set/get/delete environment variables for
   Fullscreen applications"
   homepage "https://github.com/Fullscreen/fs-env"
-  url "https://github.com/Fullscreen/fs-env/releases/download/1.0.0/darwin_amd64.zip"
+  url "https://github.com/Fullscreen/fs-env/releases/download/1.1.0/darwin_amd64.zip"
   version "1.1.0"
   sha256 "70b7682ba2ccd06d1a13e8d574aa1de4db1f820c0a2008963bbf221288162f62"
 
