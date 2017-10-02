0bdd5b43280f4b9a88fe59a6bced58cdc59ecb55
Jeremy Uziel <jeremy.uziel@fullscreen.com>
2017-10-02 11:51:30 -0700

    new version that only does 2 cli calls instead of 3.

--- aws-scale.rb
+++ aws-scale.rb
@@ -1,9 +1,9 @@
 class  AwsScale < Formula
   desc "Convenience bash function for AWS autoscaling on EC2"
   homepage "https://github.com/juziel/aws-scale"
-  url "https://github.com/juziel/aws-scale/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "c9c69896f9577bf84ea770aa6fe23f74e1d6a00410d5976b1d6a421058174827"
+  url "https://github.com/juziel/aws-scale/archive/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "2afbc1a7ffd691dc120fd6f7aa30d08fdbec083bdd1b34f2528d5153464da9d5"
 
   def install
     bin.install "aws-scale"
