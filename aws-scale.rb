6f4f4ebdcd8816eb88c8a8e659eec67298157c98
Jeremy Uziel <jeremy.uziel@fullscreen.com>
2017-10-02 17:18:07 -0700

    minor fix

--- aws-scale.rb
+++ aws-scale.rb
@@ -1,9 +1,9 @@
 class  AwsScale < Formula
   desc "Convenience bash function for AWS autoscaling on EC2"
   homepage "https://github.com/juziel/aws-scale"
-  url "https://github.com/juziel/aws-scale/archive/1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "2afbc1a7ffd691dc120fd6f7aa30d08fdbec083bdd1b34f2528d5153464da9d5"
+  url "https://github.com/juziel/aws-scale/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "9b98779bd0b96b9a38d32666d4280fc83d0b0934d6bacccc91e08cfea7a8604c"
 
   def install
     bin.install "aws-scale"
