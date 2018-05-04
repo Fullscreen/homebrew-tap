c9a3e81e1dd468789e012d99f2a0bc3f6246cb7e
Drew Stokes <drew@drewstokes.com>
2018-05-04 13:58:04 -0700

    ecs-local 0.0.7

--- ecs-local.rb
+++ ecs-local.rb
@@ -1,9 +1,9 @@
 class EcsLocal < Formula
   desc "Run ECS task definitions locally"
   homepage "https://github.com/Fullscreen/ecs-local"
-  url "https://github.com/Fullscreen/ecs-local/releases/download/0.0.6/darwin_amd64.zip"
-  version "0.0.6"
-  sha256 "5ce00b70287afa70bdae096c7afdc4a847c4f7c600ed041fe5bff0a3ab7b0049"
+  url "https://github.com/Fullscreen/ecs-local/releases/download/0.0.7/darwin_amd64.zip"
+  version "0.0.7"
+  sha256 "462dab74a2ee26b18714fa893f1c40d0a2ae00c3d6423fa5fcc099be3e5fc3f3"
 
   def install
     bin.install "ecs-local"
