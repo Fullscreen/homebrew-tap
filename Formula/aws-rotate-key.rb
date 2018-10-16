1bf01adfd429b831eb0090c39a18f67ddb0ad7d2
Borislav Ivanov <borogl@gmail.com>
2018-10-16 15:14:57 -0700

    Add aws-rorate-key version

--- Formula/aws-rotate-key.rb
+++ Formula/aws-rotate-key.rb
@@ -1,6 +1,7 @@
 class AwsRotateKey < Formula
   desc "Easily rotate your AWS key."
   homepage "https://github.com/Fullscreen/aws-rotate-key"
+  version "1.0.4"
   url "https://github.com/Fullscreen/aws-rotate-key/releases/download/v1.0.4/aws-rotate-key-1.0.4-darwin_amd64.zip"
   sha256 "c88e2caf4cf9a740020c824050de01322fcb2875116754bf7894ccfc4f3b2a57"
 
