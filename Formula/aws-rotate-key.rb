e591154c6f4095afc7e005a78cc4e11c2986e6b7
Borislav Ivanov <borogl@gmail.com>
2018-12-23 15:59:48 -0800

    Bump aws-rotate-key to v1.0.5

--- Formula/aws-rotate-key.rb
+++ Formula/aws-rotate-key.rb
@@ -1,9 +1,9 @@
 class AwsRotateKey < Formula
   desc "Easily rotate your AWS key."
   homepage "https://github.com/Fullscreen/aws-rotate-key"
-  url "https://github.com/Fullscreen/aws-rotate-key/releases/download/v1.0.4/aws-rotate-key-1.0.4-darwin_amd64.zip"
+  url "https://github.com/Fullscreen/aws-rotate-key/releases/download/v1.0.5/aws-rotate-key-1.0.5-darwin_amd64.zip"
   version "1.0.4"
-  sha256 "c88e2caf4cf9a740020c824050de01322fcb2875116754bf7894ccfc4f3b2a57"
+  sha256 "c10a4812f049af0b3b04c28936a9c1ad92623f0a0031f00e41ee4fd5c8aa70f9"
 
   def install
     bin.install "aws-rotate-key"
