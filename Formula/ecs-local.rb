9bae43417e803e4bf78a3db869cc50773114aa5a
Scott Stout <scott.stout@fullscreen.net>
2019-01-22 15:35:08 -0800

    ecs-local pry logging

--- Formula/ecs-local.rb
+++ Formula/ecs-local.rb
@@ -1,9 +1,9 @@
 class EcsLocal < Formula
   desc "Run ECS task definitions locally"
   homepage "https://github.com/Fullscreen/ecs-local"
-  url "https://github.com/Fullscreen/ecs-local/releases/download/v0.1.1/ecs-local_Darwin_x86_64.tar.gz"
-  version "0.1.1"
-  sha256 "164181f3f2c44fe6ff9d369fc7d95520a0f8c5616b4c121b1d67471ec441be84"
+  url "https://github.com/Fullscreen/ecs-local/releases/download/v0.1.2/ecs-local_Darwin_x86_64.tar.gz"
+  version "0.1.2"
+  sha256 "122b05e3d618196616461b955b4f9e5a12f1e305d34d8853b6dab7050e3608c5"
 
   def install
     bin.install "ecs-local"
