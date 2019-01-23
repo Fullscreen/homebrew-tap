7a29847168e95ac987567d8c0930b64c0483589b
devops <devops@fullscreen.com>
2019-01-23 01:02:46 -0800

    Brew formula update for ecs-local version v0.1.5

--- Formula/ecs-local.rb
+++ Formula/ecs-local.rb
@@ -1,11 +1,11 @@
 class EcsLocal < Formula
   desc "Run ECS task definitions locally"
   homepage "https://github.com/Fullscreen/ecs-local"
-  url "https://github.com/Fullscreen/ecs-local/releases/download/v0.1.3/ecs-local_Darwin_x86_64.tar.gz"
-  version "0.1.3"
-  sha256 "a19821580d0ad45f618a2bf703075a72ce6710e302f8291ca8f06750e62b700f"
+  url "https://github.com/Fullscreen/ecs-local/releases/download/v0.1.5/ecs-local_Darwin_x86_64.tar.gz"
+  version "0.1.5"
+  sha256 "ab5cc0c9cda95f173fc5a56458d2773bc272c0c0d971bfbff34723c57c4e13ec"
 
   def install
-    bin.install "ecs-local"
+    bin.install ""ecs-local""
   end
 end
