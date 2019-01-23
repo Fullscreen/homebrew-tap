45815629588cead77a65a4cf1b70656c95860470
Scott Stout <scott.stout@fullscreen.net>
2019-01-22 16:17:04 -0800

    ecs-local mounts

--- Formula/ecs-local.rb
+++ Formula/ecs-local.rb
@@ -1,9 +1,9 @@
 class EcsLocal < Formula
   desc "Run ECS task definitions locally"
   homepage "https://github.com/Fullscreen/ecs-local"
-  url "https://github.com/Fullscreen/ecs-local/releases/download/v0.1.2/ecs-local_Darwin_x86_64.tar.gz"
-  version "0.1.2"
-  sha256 "122b05e3d618196616461b955b4f9e5a12f1e305d34d8853b6dab7050e3608c5"
+  url "https://github.com/Fullscreen/ecs-local/releases/download/v0.1.3/ecs-local_Darwin_x86_64.tar.gz"
+  version "0.1.3"
+  sha256 "a19821580d0ad45f618a2bf703075a72ce6710e302f8291ca8f06750e62b700f"
 
   def install
     bin.install "ecs-local"
